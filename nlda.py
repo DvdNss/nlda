ba1797a914b90608a8dae8de1603885a1001a25c
David NAISSE <private.david.naisse@gmail.com>
2021-09-20 23:29:53 +0200

    Added ld model path variable

--- nlda.py
+++ nlda.py
@@ -1,8 +1,10 @@
 from language_detection.language_detector import load_model, detect_language
 
+ld_path = 'language_detection/model/lid.176.ftz'
+
 # Load language detector
-language_detector = load_model('language_detection/model/lid.176.ftz')
+language_detector = load_model(ld_path)
 
 # Detect language
-result = detect_language(['this is a test'], language_detector=language_detector)
+result = detect_language(['this is a test', 'mi chiamo DAvide sono francese'], language_detector=language_detector)
 print(result['stats'])
