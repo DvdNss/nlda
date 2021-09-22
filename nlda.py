a832ea58223ec1dd6c9b4648b3372884e78f65ab
David NAISSE <private.david.naisse@gmail.com>
2021-09-22 17:25:03 +0200

    Added TODO

--- nlda.py
+++ nlda.py
@@ -8,3 +8,5 @@ language_detector = load_model(ld_path)
 # Detect language
 result = detect_language(['this is a test', 'mi chiamo DAvide sono francese'], language_detector=language_detector)
 print(result['stats'])
+
+# TODO: translation and paraphrasing
