f76acb1ca2f4dabfbf97e6187d5c6beed6e69eaa
David NAISSE <private.david.naisse@gmail.com>
2021-09-29 11:08:53 +0200

    updated main script and ld submodule version

--- nlda.py
+++ nlda.py
@@ -1,15 +1,32 @@
+import logging
+
+import pandas
+
 from helsinki_translation.helsinki_translation import load_model as load_translator, translate
 from helsinki_translation.languages import Language
-from language_detection.language_detector import load_model as load_detector, detect_language
+from language_detection.language_detector import load_model as load_detector, detect_language_from_df
 
-ld_path = 'language_detection/model/lid.176.ftz'
+logging.basicConfig(level=logging.INFO)
 
 # Load language detector
-language_detector = load_detector(ld_path)
+language_detector = load_detector()
+
+# Load DataFrame
+dataset = pandas.read_csv('df.tsv', sep='\t').astype(str)
 
 # Detect language
-result = detect_language(['this is a test', 'mi chiamo davide sono francese'], language_detector=language_detector)
-print(result['stats'])
+dataset, stats = detect_language_from_df(dataset, language_detector=language_detector)
+logging.info(f" Dataset stats are the following:\n{stats}")
+
+target_languages = ['en', 'fr', 'it']
+
+# Add id column to track translations
+dataset['id'] = ''
+data_to_add = []
+
+for index, row in dataset.iterrows():
+    row['id'] = index
+    data_to_add.append([])
 
 # Load translator
 tokenizer, translator = load_translator(source=Language.English, target=Language.French)
@@ -17,5 +34,3 @@ tokenizer, translator = load_translator(source=Language.English, target=Language
 # Translate some text
 translation = translate(source='Hi, my name is David.', tokenizer=tokenizer, model=translator)
 print(translation)
-
-# TODO: paraphrasing
