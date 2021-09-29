a23dbcd2f48c21836f23e951d786aff5ffb748ee
David NAISSE <private.david.naisse@gmail.com>
2021-09-29 10:01:57 +0200

    updated ht submodule version

--- nlda.py
+++ nlda.py
@@ -1,5 +1,6 @@
 from language_detection.language_detector import load_model as load_detector, detect_language
 from helsinki_translation.helsinki_translation import load_model as load_translator, translate
+from helsinki_translation.languages import Language
 
 ld_path = 'language_detection/model/lid.176.ftz'
 
@@ -11,7 +12,7 @@ result = detect_language(['this is a test', 'mi chiamo davide sono francese'], l
 print(result['stats'])
 
 # Load translator
-tokenizer, translator = load_translator(source='en', target='fr')
+tokenizer, translator = load_translator(source=Language.English, target=Language.French)
 
 # Translate some text
 translation = translate(source='Hi, my name is David.', tokenizer=tokenizer, model=translator)
