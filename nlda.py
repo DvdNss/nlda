161bcdf539e5c2257e6710ab274dc9881a2c2b7f
David NAISSE <private.david.naisse@gmail.com>
2021-09-29 13:34:15 +0200

    updated main script

--- nlda.py
+++ nlda.py
@@ -1,12 +1,14 @@
 import logging
 
 import pandas
+from tqdm import tqdm
 
 from helsinki_translation.helsinki_translation import load_model as load_translator, translate
 from helsinki_translation.languages import Language
 from language_detection.language_detector import load_model as load_detector, detect_language_from_df
 
 logging.basicConfig(level=logging.INFO)
+logging.info('Currently running NLDA. \n')
 
 # Load language detector
 language_detector = load_detector()
@@ -16,21 +18,38 @@ dataset = pandas.read_csv('df.tsv', sep='\t').astype(str)
 
 # Detect language
 dataset, stats = detect_language_from_df(dataset, language_detector=language_detector)
-logging.info(f" Dataset stats are the following:\n{stats}")
+logging.info(f" Dataset stats are the following:\n{stats}\n")
 
 target_languages = ['en', 'fr', 'it']
 
 # Add id column to track translations
 dataset['id'] = ''
-data_to_add = []
-
 for index, row in dataset.iterrows():
     row['id'] = index
-    data_to_add.append([])
 
-# Load translator
-tokenizer, translator = load_translator(source=Language.English, target=Language.French)
+data_to_add = []
+id_count = 0
+translate_targets = False
 
-# Translate some text
-translation = translate(source='Hi, my name is David.', tokenizer=tokenizer, model=translator)
-print(translation)
+logging.info(f' Translating your dataset to {target_languages}. Please wait...')
+for source_language in tqdm(stats.keys()):
+    for target_language in target_languages:
+        if source_language != target_language:
+
+            # Load source-target pair model
+            try:
+                tokenizer, translator = load_translator(source=source_language, target=target_language)
+
+                for index, row in dataset.iterrows():
+                    if row['language'] == source_language:
+                        data_to_add.append([
+                            translate(source=row['source_text'], tokenizer=tokenizer, model=translator),
+                            translate(source=row['target_text'], tokenizer=tokenizer,
+                                      model=translator) if translate_targets is True else row['target_text'],
+                            target_language,
+                            row['id']
+                        ])
+            except:
+                pass
+
+print(data_to_add)
